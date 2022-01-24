38c23d737c662533ac96a0c91751dfdb3dc5ebb3
softrebel <sh.mohammad66@gmail.com>
2022-01-24 09:16:20 +0330

    extract communities edge list

--- src/tolv.py
+++ src/tolv.py
@@ -1,4 +1,7 @@
-
+import json
+output={
+    'windows':[]
+}
 names=[
     'ged_irandoc_one_mode_tf_1300_1372.graphml',
     'ged_irandoc_one_mode_tf_1373_1377.graphml',
@@ -8,6 +11,7 @@ names=[
     'ged_irandoc_one_mode_tf_1395_1399.graphml',
 ]
 for name in names:
+    temp = {'communities': []}
     from igraph import Graph,plot
     from bidi.algorithm import get_display
     from arabic_reshaper import reshape
@@ -24,9 +28,10 @@ for name in names:
     visual_style["bbox"] = (2400, 1800)
     visual_style["margin"] = 100
     visual_style["vertex_label"] = g.vs['label']
-
+    partition.subgraphs()
     g.vs["group"] = partition.membership
-
+    temp['communities'] = [[[x.vs[z]['name'] for z in e.tuple]
+                            for e in x.es] for x in partition.subgraphs()]
 
     print(f'modularity louvain {name}: {partition.modularity}')
 
@@ -38,6 +43,10 @@ for name in names:
     # plot(partition,  **visual_style)
     plot(partition, f'{name}.svg', **visual_style)
     # plot(clusters2, 'c_edge_betweenness.png', **visual_style)
+    output['windows'].append(temp)
+    
+with open('louvain_output.json', 'w',encoding='utf-8') as f:
+    json.dump(output, f, indent=4,ensure_ascii=False)
 
 
 
